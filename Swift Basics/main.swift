cb7d609ee8587d0f87a2ba8813853f28649e4c74
grantharrison <gthgg6@mail.missouri.edu>
2019-09-05 13:44:03 -0500

    Added varibles and some if statements

--- Swift Basics/main.swift
+++ Swift Basics/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+// Variables
+let sample1: UInt8 = 0x3A
+var sample2: UInt8 = 58
+var heartRate: Int = 85
+var deposits: Double = 135002796
+let acceleration: Float = 9.800
+var mass: Float = 14.6
+var distance: Double = 129.763001
+var lost: Bool = true
+var expensive: Bool = true
+var choice: Int = 2
+let integral: Character = "\u{222B}"
+let greeting: String = "Hello"
+
+//Compare sample1 to sample2
+if sample1 == sample2 {
+    print("The samples are equal.")
+}
+else {
+    print("The samples are not equal.")
+}
+// Heart Rate greater
+if heartRate > 40 && heartRate <= 80 {
+    print("Heart rate is normal.")
+}
+else {
+    print("Heart rate is not normal.")
+}
+
+
+
 
