51c644200898238e23fb2c4de74cbf99d98cc574
grantharrison <gthgg6@mail.missouri.edu>
2019-09-05 13:15:07 -0500

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Grant Harrison on 9/5/19.
+//  Copyright © 2019 Grant Harrison. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
